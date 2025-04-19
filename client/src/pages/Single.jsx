8b5f4513655031a588db00abed7f1935474002b2
Ananya Srivastava <ananyasrivastava419@gmail.com>
2025-04-19 17:43:24 +0530

    lazy commit

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Single = () => {
   return (
     <div className='single'>
-      <div className="content">c</div>
+      <div className="content">
+        <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="user">
+        <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+        <div className="info">
+          <span>John</span>
+          <p>Posted 2 days ago</p>
+        </div>
+        <div className="edit">
+          <img src='https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=600' alt="" />
+        </div>
+        </div>
+      </div>
       <div className="menu">m</div>
 
     </div>
